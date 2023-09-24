695655f6fa5576a51ce2e54d19653a6e22025567
João Vitor <joaovitormdesa@gmail.com.br>
2023-09-24 18:53:05 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using FatoresPrimos.Services;
+
+
+namespace FatoresPrimos.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MathService _mathServices;
+
+        public HomeController()
+        {
+            _mathServices = new MathService();
+        }
+
+        public ActionResult Index(int numero)
+        {
+
+            if(numero == 0)
+                return Json(new { numero = numero, mensagem = "Desculpe, zero não tem nenhum divisor!" }, JsonRequestBehavior.AllowGet);
+
+            var divisores = _mathServices.Decompor(numero);
+            var divisoresPrimos = _mathServices.Primos(divisores);
+
+            return Json(new { numero, divisores, divisoresPrimos }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
+
---
+++ Service/MathServices.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace FatoresPrimos.Services
+{
+    public class MathService
+    {
+        public List<int> Decompor(int number)
+        {
+
+            List<int> divisores = new List<int>();
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    divisores.Add(i);
+                }
+            }
+
+            return divisores;
+
+        }
+
+        public List<int> Primos(List<int> numbers)
+        {
+            List<int> primos = new List<int>();
+
+            foreach (int number in numbers)
+            {
+                int divisores = 0;
+
+                for (int i = 1; i <= number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        divisores++;
+                        if (divisores > 2)
+                            break;
+                    }
+                }
+
+                if (divisores == 2)
+                    primos.Add(number);
+                
+            }
+
+
+            return primos;
+        }
+
+    }
+}
+
